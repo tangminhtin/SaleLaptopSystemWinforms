a429ad988855b0d7381ae7004193cfc5b1ab2970
Nicky Hua <quocvinh12360@gmail.com>
2020-11-02 16:47:21 +0700

    thay doi hien thi loi tren form

--- PRN292_LapTopSaleSystemWF_Group4/PRN292_LapTopSaleSystemWF_Group4/Validate/AuthenticationValidate.cs
+++ PRN292_LapTopSaleSystemWF_Group4/PRN292_LapTopSaleSystemWF_Group4/Validate/AuthenticationValidate.cs
@@ -39,78 +39,23 @@ namespace PRN292_LapTopSaleSystemWF_Group4.Validate
 
         public Boolean checkPhone(String input)
         {
-            try
-            {
                 String phonematch = @"^(\d{3})[ -]?(\d{3})[ -]?(\d{4}) x(\d*)";
-                return Regex.Match(input, phonematch).Success;
-            }catch(Exception e)
-            {
+                if(Regex.Match(input, phonematch).Success)
+                {
+                    return true;
+                }
                 return false;
-            }
         }
 
-        public String checkPassword(String password)
-        {
-            /*int score = 0;
+        public Boolean checkPassword(String password)
+        {       
+            string rex = @"^(?=.*[a - z])(?=.*[A - Z])(?=.*\d)(?=.*[^\da - zA - Z]).{ 8,15}$"; //match digits
 
-            if (password.Length < 1)
-                return "Blank";
-            if (password.Length < 4)
-                return "VeryWeak";
-
-            if (password.Length >= 8)
-                score++;
-            if (password.Length >= 12)
-                score++;
-            if (Regex.Match(password, @"/\d+/", RegexOptions.ECMAScript).Success)
-                score++;
-            if (Regex.Match(password, @"/[a-z]/", RegexOptions.ECMAScript).Success &&
-              Regex.Match(password, @"/[A-Z]/", RegexOptions.ECMAScript).Success)
-                score++;
-            if (Regex.Match(password, @"/.[!,@,#,$,%,^,&,*,?,_,~,-,£,(,)]/", RegexOptions.ECMAScript).Success)
-                score++;
-
-            switch (score)
+            if(Regex.Match(password, rex).Success)
             {
-                case 1:
-                    return "VeryWeak";
-                    break;
-                case 2:
-                    return "Weak";
-                    break;
-                case 3:
-                    return "Medium";
-                    break;
-                case 4:
-                    return "Strong";
-                    break;
-                case 5:
-                    return "VeryStrong";
-                    break;
-                default:
-                    return "";
-                    
-            }*/
-
-            
-            string patdi = @"\d+"; //match digits
-            string patupp = @"[A-Z]+"; //match upper cases
-            string patlow = @"[a-z]+"; //match lower cases
-            string patsym = @"[`~!@$%^&\\-\\+*/_=,;.':|\\(\\)\\[\\]\\{\\}]+"; //match symbols
-
-            Match id = Regex.Match(password, patdi);
-            Match upp = Regex.Match(password, patupp);
-            Match low = Regex.Match(password, patlow);
-            Match sym = Regex.Match(password, patsym);
-
-            if(id.Success && upp.Success && low.Success && sym.Success)
-            {
-                return "VeryStrong";
+                return true;
             }
-            else
-            {
-                return "";
-            }
+            return false;
            
         }
     }
